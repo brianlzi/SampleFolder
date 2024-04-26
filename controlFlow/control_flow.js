a1ba8e42d9cae73d0991683288791f0e79c8d111
Brian <giangnv.soict.hust@gmail.com>
2024-04-26 04:08:48 -0400

    Add control flow

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,47 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access level: ", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+
+} else {
+    userMessage = "Please log in to access the system";
+}
+
+console.log("User message: ", userMessage);
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subcriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("User category: ", userCategory);
